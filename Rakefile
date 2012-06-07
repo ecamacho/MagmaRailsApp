3e1d29aecbf2e8a70c042cf2503f399dc1a66aee
Erick Camacho <ecamacho.mx@gmail.com>
2012-06-07 09:56:10 -0500

    added nib files, speakers table

--- Rakefile
+++ Rakefile
@@ -5,4 +5,5 @@ Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'MagmaRails'
   app.frameworks += ['CoreLocation', 'MapKit']
+  app.codesign_certificate = 'iPhone Developer: Erick Camacho (V2NZW66M9L)'
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,8 +2,8 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)  	
   	tabbar = UITabBarController.alloc.init
-
-    tabbar.viewControllers = [aboutController, scheduleController, venueController, sponsorsController]
+    tabbar.tabBar.tintColor = UIColor.colorWithRed( 255/100, green:0.0, blue:0.0, alpha:1.0)
+    tabbar.viewControllers = [aboutController, scheduleController, venueController, speakersController]
     tabbar.selectedIndex = 0
     @window.rootViewController = tabbar    
     @window.makeKeyAndVisible
@@ -11,27 +11,34 @@ class AppDelegate
   end
 
   def aboutController
-  	aboutController = AboutController.alloc.init
-  	
-    aboutController.tabBarItem = UITabBarItem.alloc.initWithTitle('Acerca', image:nil, tag:1)
-  	aboutController
+  	aboutController = AboutController.alloc.initWithNibName('AboutView', bundle:nil)  	
+    
+  	navController = UINavigationController.alloc.initWithRootViewController aboutController
+    navController.tabBarItem = UITabBarItem.alloc.initWithTitle('Magma', image:UIImage.imageNamed('magma_tab.png'), tag:1)
+    navController.navigationBar.tintColor = UIColor.colorWithRed( 255/100, green:0.0, blue:0.0, alpha:1.0)
+    navController    
   end	
 
   def scheduleController
     scheduleController = ScheduleController.alloc.init    
-    scheduleController.tabBarItem = UITabBarItem.alloc.initWithTitle('Agenda', image:nil, tag:2)
-    scheduleController
+    
+    navController = UINavigationController.alloc.initWithRootViewController scheduleController
+    navController.tabBarItem = UITabBarItem.alloc.initWithTitle('Agenda', image:UIImage.imageNamed('calendar.png'), tag:2)
+    navController    
   end 
 
-  def sponsorsController
-    sponsorsController = SponsorsController.alloc.init    
-    sponsorsController.tabBarItem = UITabBarItem.alloc.initWithTitle('Sponsors', image:nil, tag:3)
-    sponsorsController
+  def speakersController
+    speakersController = SpeakersController.alloc.init
+    navController = UINavigationController.alloc.initWithRootViewController speakersController
+    navController.tabBarItem = UITabBarItem.alloc.initWithTitle('Speakers', image:UIImage.imageNamed('bullhorn.png'), tag:3)
+    navController    
   end 
 
   def venueController
     venueController = VenueController.alloc.init    
-    venueController.tabBarItem = UITabBarItem.alloc.initWithTitle('Venue', image:nil, tag:4)
-    venueController
+    navController = UINavigationController.alloc.initWithRootViewController venueController    
+    navController.tabBarItem = UITabBarItem.alloc.initWithTitle('Venue', image:UIImage.imageNamed('map.png'), tag:4)
+    
+    navController    
   end 
 end
